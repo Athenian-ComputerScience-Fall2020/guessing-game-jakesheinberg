f096bbe2a9b05afff3721b21e01b21a76baca964
jakesheinberg <23jsheinberg@athenian.org>
2020-09-17 06:02:59 +0000

    working but not tested

--- game1.py
+++ game1.py
@@ -8,4 +8,25 @@ Use this file to write an "open" version of the game (no test code or defined fo
 "I'm sorry, you are out of guesses."
 
 '''
-# Collaborators: 
+# Collaborators: none
+
+import random
+
+answer=random.randint(0,10)
+
+guessnumber=0
+while guessnumber <5:
+    guess=int(input("Guess a number 1 through 10:  "))
+    guessnumber=guessnumber+1
+    if guess==answer:
+        print(f"You Got it on your {guessnumber} attempt!")
+        break
+    elif guess<answer:
+        print("Your number is too low.")
+    elif guess>answer:
+        print("Your number is too high.")
+    elif guess==5:
+        print("I'm sorry, you are out of guesses.")
+
+
+
