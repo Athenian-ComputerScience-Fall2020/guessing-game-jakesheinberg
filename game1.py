47fc74c3997ad905be8792bd848a939957703b27
jakesheinberg <23jsheinberg@athenian.org>
2020-09-24 17:19:56 +0000

    must finish smooth loop

--- game1.py
+++ game1.py
@@ -9,34 +9,47 @@ Use this file to write an "open" version of the game (no test code or defined fo
 
 '''
 # Collaborators: none
-
 import random
+def guessing_game():
+    start_rang=int(input("What would you like the range to start at: "))
+    end_rang=int(input("What would you like the range to end at: "))
+    choices=end_rang-start_rang+1
+    guesses_avail=int(input(f"There are {choices} numbers to guess from. How many guesses would you like: "))
+    answer=random.randint(start_rang,end_rang+1)
 
-answer=random.randint(0,10)
+    guessnumber=0
+    while guessnumber <guesses_avail:
+        guess=(input(f"Guess a number between {start_rang} and {end_rang} or type 'done' to quit: :  "))
+        try:
+            guess=int(guess)
+            guessnumber=guessnumber+1
+        except:
+            if guess=="done":
+                print("You have quit. Come again soon!")
+                break
+            else:
+                print("Please pick a number 1 through 10")
 
-guessnumber=0
-while guessnumber <5:
-    try:
-        guess=int(input("Guess a number 1 through 10  or type '0' to quit: :  "))
+            print("please pick a number 1 through 10")
         guessnumber=guessnumber+1
-    except:
-        print("please pick a number 1 through 10")
-    guessnumber=guessnumber+1
-    if guess == 0:
-        print("you have quit")
-        break
-       
-    elif guess==answer:
-        print(f"You Got it on your {guessnumber} attempt!")
-        break
-    elif 0<guess<answer:
-        print("Your number is too low.")
-    elif 11>guess>answer:
-        print("Your number is too high.")
-    elif guess==5:
-        print("I'm sorry, you are out of guesses.")
-    else:
-        print("Answer is out of range.")
+        if guess==answer:
+            print(f"You Got it on your {guessnumber} attempt!")
+            break
+        elif 0<guess<answer:
+            print("Your number is too low.")
+        elif 11>guess>answer:
+            print("Your number is too high.")
+        elif guess==5:
+            print("I'm sorry, you are out of guesses.")
+        else:
+            print("Answer is out of range.")
 
 
 
+guessing_game()
+playagain=input("Would you like to play again: ")
+playagain=playagain.upper()
+if playagain== "YES" or "YA":
+    guessing_game()
+else:
+    break
